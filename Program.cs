a82932a94b020acc31dbce932de4c1e29c2565fa
Josh McCurry <joshmccurry@microsoft.com>
2022-12-28 14:53:35 -0500

    Add AppInsights

--- Program.cs
+++ Program.cs
@@ -1,13 +1,29 @@
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.ApplicationInsights;
 using Microsoft.Azure.Functions.Worker;
+using Grpc.Core;
 
 namespace dotnet_isolated_http_example {
     class Program {
         static async Task Main(string[] args) {
             var host = new HostBuilder()
-            .ConfigureFunctionsWorkerDefaults()
+            .ConfigureFunctionsWorkerDefaults() //Load Worker Defaults
+
+            .ConfigureLogging((context, builder) => {
+                builder.AddApplicationInsights(
+                    //Add the AI connection string to the local.settings.json and Azure app settings
+                    //
+                    configureTelemetryConfiguration: (config) => config.ConnectionString = context.Configuration["APPLICATIONINSIGHTS_CONNECTION_STRING"],
+                    configureApplicationInsightsLoggerOptions: (options) => { }
+                );
+
+                // Capture all log-level entries from Program
+                builder.AddFilter<ApplicationInsightsLoggerProvider>(
+                    typeof(Program).FullName, LogLevel.Trace);
+
+            })
             .Build();
 
             await host.RunAsync();
